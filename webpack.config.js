5e5559185d6a6ddd1a3e998b2fdb77ed3fd8d5e8
Miguel Malagon <miguelmalagon@outlook.com>
2019-07-22 13:34:16 -0500

    add  react and standerd as lint to pretty format code

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	output: {
+		filename: 'app.bundle.js'
+	},
+	plugins: [new htmlWebpackPlugin({ template: 'src/index.html' })],
+	module: {
+		rules: [
+			{
+				// Rule to run the last features on ecma on my site
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env', '@babel/preset-react']
+					}
+				}
+			}
+		]
+	}
+};
