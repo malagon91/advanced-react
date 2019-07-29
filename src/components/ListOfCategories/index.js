f0e2cc8e20fd060324684443cc73fd8d8244529c
Miguel Malagon <miguelmalagon@outlook.com>
2019-07-29 17:57:44 -0500

    add styles on categories

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,8 +1,50 @@
-import React from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
 import { List, Item } from './styles'
 import { Category } from '../Category'
-import { categories } from '../../../api/db.json'
 
-export const ListOfCategories = () => (<List>
-  {categories.map(cat => <Item key={cat.id} > <Category {...cat} /></Item>)}
-</List>)
+function useCategoriesData () {
+  const [categories, setCategories] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+    window
+      .fetch('https://petgram-miguel-api.now.sh/categories')
+      .then(res => res.json())
+      .then(response => {
+        setCategories(response)
+        setLoading(false)
+      })
+  }, [])
+  return { categories, loading }
+}
+
+export const ListOfCategories = () => {
+  const [showFixed, setShowFixed] = useState(false)
+  const { categories, loading } = useCategoriesData()
+  useEffect(() => {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed()
+    }
+    document.addEventListener('scroll', onScroll)
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = fix => (
+    <List fixed={fix}>
+      {loading ? <Item key='loading' ><Category /></Item> : categories.map(cat => (
+        <Item key={cat.id}>
+          <Category {...cat} />
+        </Item>
+      ))}
+    </List>
+  )
+
+  return (
+    <Fragment>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </Fragment>
+  )
+}
