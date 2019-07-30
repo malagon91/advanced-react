343236a8a390cc1f5d777cd1163ce67cc3ef709d
Miguel Malagon <miguelmalagon@outlook.com>
2019-07-30 17:13:03 -0500

    add multiple custom hooks

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,37 +1,15 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React from 'react'
 import { ImgWrapper, Img, Button, Article } from './styles'
 import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
 import { useLocalStorage } from '../../hooks/useLocalStorage'
+import { useNearScreen } from '../../hooks/useNearScreen'
 const DEFAULT_PHOTO =
   'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
-
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_PHOTO }) => {
-  const element = useRef(null)
   const key = `like-${id}`
-
-  const [show, setShow] = useState(false)
   const [liked, setliked] = useLocalStorage(key, false)
-
-  useEffect(() => {
-    // Se agrega intersection oberver para lazy loading pero no todos los nav lo usan asi que se cargar el babel para soportarlo             plugins: ['@babel/plugin-syntax-dynamic-import'],
-    // Como en chrome si funciona se agrupa todo en una promesa para que solo lo traiga cuando el nav no lo tenga si lo tiene solo lo usamos
-    // se usa un wrapper porque el import() es una promesa
-    Promise.resolve(
-      typeof window.IntersectionObserver !== 'undefined'
-        ? window.IntersectionObserver
-        : import('intersection-observer')
-    ).then(() => {
-      const observer = new window.IntersectionObserver(entries => {
-        const { isIntersecting } = entries[0]
-        if (isIntersecting) setShow(true)
-        observer.disconnect()
-      })
-      // Lazy load para cargar las paginas
-      // Logic to check if the element is in the viewport
-      observer.observe(element.current)
-    })
-  }, [element])
+  const [show, element] = useNearScreen()
 
   const Icon = liked ? MdFavorite : MdFavoriteBorder
 
