110bbfda011994b987c17197bc330ace0acaf8c3
Miguel Malagon <miguelmalagon@outlook.com>
2019-07-31 10:48:52 -0500

    add hoc and container

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -1,23 +1,8 @@
 import React from 'react'
-import { graphql } from 'react-apollo'
-import { gql } from 'apollo-boost'
 import { PhotoCard } from '../PhotoCard'
+import { withPhotos } from '../../hoc/withPhotos'
 
-const withPhotos = graphql(gql`
-query getPhotos($categoryId: ID) {
-    photos(categoryId: $categoryId){
-        id
-        categoryId
-        src
-        likes
-        userId
-        liked
-    }
-}
-`)
-
-const ListOfPhotoCardsComponent = ({ data: { photos = [] } } = {}) => <ul>
+export const ListOfPhotoCardsComponent = ({ data: { photos = [] } } = {}) => <ul>
   {photos.map(photo => <PhotoCard key={photo.id} {...photo} />)}
 </ul>
 
-export const ListOfPhotoCards = withPhotos(ListOfPhotoCardsComponent)
